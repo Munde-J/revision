4b7ec8ee008cc8c57df350850fba4cdf8b7f4940
Munde-J <mundeangelah@gmail.com>
2022-04-14 09:57:11 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,20 @@
+fun main(){
+    println(isEven(4))
+}
+fun isEven(x:Int):Boolean{
+    return(x%2==0)
+
+    println(evenNumber("fd","hy","chosgt"))
+}
+//fun reStr(arr:Array<String>):List<String>{
+//    return names
+//}
+fun evenNumber(fruit:String):String{
+    var x=" "
+    for (n in fruit){
+        if (fruit.indexOf(n)%2==0){
+            x+=n
+        }
+    }
+    return x
+}
